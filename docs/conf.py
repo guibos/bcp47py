7a0b3f6743f49505966b4811808a64fef01bba2c
guibos <22233599+guibos@users.noreply.github.com>
2023-10-23 18:54:43 +0200

    WIP

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-
-project = 'BCP47Py'
+project = 'bcp47py'
 copyright = '2023, Guillermo Ferrer Bosque'
 author = 'Guillermo Ferrer Bosque'
 release = 'Mozilla Public License Version 2.0'
@@ -15,33 +14,15 @@ release = 'Mozilla Public License Version 2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'autoapi.extension'
-#     'sphinx.ext.intersphinx',
-#     'sphinx.ext.viewcode',
-#     'sphinx.ext.graphviz',
-# ]
-
-extensions = [
-    'autoapi.extension',
-    'sphinx-pydantic',
-]
-autoapi_dirs = ['../../src']
-
-# autodoc_default_options = {
-#     'members': True,
-#     # Does now show base classes otherwise... why such bad defaults?
-#     # But with this it does show useless bases like `object`. What is one to do?
-#     'show-inheritance': True,
-# }
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
 html_static_path = ['_static']
-
